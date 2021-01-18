e7f4bf5a8b806244b5141788b9a9102e3d96ef1e
lhh9799 <77262005+lhh9799@users.noreply.github.com>
2021-01-19 02:01:04 +0900

    1. play 메소드의 로직 변경(newplay 라는 새로운 메소드 추가함. newplay 검증이 끝나면 play 메소드 삭제 필요) 2. 음악 다운로드, 제목 추출을 위한 클래스, 메소드 추가 3. Spotify를 통해 Artist의 Top 10 음악 제목을 가져오는 auto 메소드 추가(Visual Studio Code가 아닌 Discord에 Artist 이름을 입력하고 동작하도록 수정되어야 함) 4. 기존 소스에서는 봇이 입장하는 음성 채널을 소스에서 명시하였으나 메시지를 보낸 사용자가 접속해있는 음성 채널에 봇이 입장하도록 변경 5. 음악 재생 시작 후 현재 재생 중인 음악의 제목을 봇이 메시지로 보냄 6. queue 추가

--- projects/discord-bot/voice.py
+++ projects/discord-bot/voice.py
@@ -4,10 +4,98 @@ import youtube_dl
 import os
 
 client = commands.Bot(command_prefix="!")
+queue = list()
+
+class Song :
+    def __init__(self) :
+        ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+        }
+        self.ydl_opts = ydl_opts
+    def download_song(self, url) :
+        song_there = os.path.isfile("song.mp3")
+        try :
+            if song_there :
+                os.remove("song.mp3")
+        except PermissionError:
+            return
+
+        with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
+            ydl.download([url])
+        for file in os.listdir("./"):
+            if file.endswith(".mp3"):
+                os.rename(file, "song.mp3")
+    def get_title(self, url) :
+        with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=False)
+            video_title = info_dict.get('title', None)
+        return video_title
 
 @client.command()
 async def auto(ctx) :
-    pass
+    import fetch_playlist as fetcher
+    for var in fetcher.fetch() :
+        queue.append(var)
+    buf = 'Added song list'
+    i = 1
+    for var in queue :
+        buf = buf + '[' + str(i) + ']' + ' ' + var + '\n'
+        i += 1
+    await ctx.send(buf)
+
+@client.command()
+async def is_connected(ctx) :
+    if client.voice_clients :
+        await ctx.send("State : connected")
+    else :
+        await ctx.send("State : not connected")
+
+@client.command()
+async def newplay(ctx, url : str):
+    voiceChannel = ctx.author.voice.channel
+    
+    connection_state = False
+    # if client.voice_clients :
+    #     connection_state = True
+    
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    
+    if connection_state is True and not voice.is_playing() :    #기존 파일 삭제, 다운로드, 재생, nowplaying()
+        song_manager = Song()
+        song_manager.download_song(url)
+        video_title = song_manager.get_title(url)
+        voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+        voice.play(discord.FFmpegPCMAudio("song.mp3"))
+        queue.append(video_title)
+        await ctx.send("Now playing : {}" .format(str(queue[0]))) 
+    elif connection_state is True and voice.is_playing() :
+        song_manager = Song()
+        new_song_title = song_manager.get_title(url)
+        queue.append(new_song_title)
+        await ctx.send("Now playing : {}" .format(str(queue[0])))
+        await ctx.send("Added new song : {}" .format(new_song_title))
+    elif connection_state is False :                            #음성채널 연결, 다운로드, 재생
+        await voiceChannel.connect()
+        voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+        song_manager = Song()
+        song_manager.download_song(url)
+        video_title = song_manager.get_title(url)
+        voice.play(discord.FFmpegPCMAudio("song.mp3"))
+        queue.append(video_title)
+        await ctx.send("Now playing : {}" .format(str(queue[0])))
+    
+
+@client.command()
+async def nowplaying(ctx) :
+    if queue :
+        await ctx.send("Now playing : {}" .format(str(queue[0])))
+    else :
+        await ctx.send("Queue is empty")
 
 @client.command()
 async def play(ctx, url : str):
@@ -19,7 +107,8 @@ async def play(ctx, url : str):
         await ctx.send("Wait for the current playing music to end or use the 'stop' command")
         return
 
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
+    # voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
+    voiceChannel = ctx.author.voice.channel
     await voiceChannel.connect()
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
 
@@ -33,20 +122,29 @@ async def play(ctx, url : str):
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
+        info_dict = ydl.extract_info(url, download=False)
+        video_title = info_dict.get('title', None)
+        queue.append(video_title)
     for file in os.listdir("./"):
         if file.endswith(".mp3"):
             os.rename(file, "song.mp3")
     voice.play(discord.FFmpegPCMAudio("song.mp3"))
+    nowplaying(ctx)
 
 
 @client.command()
 async def leave(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_connected():
-        await voice.disconnect()
-    else:
-        await ctx.send("The bot is not connected to a voice channel.")
+    # voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    # if voice.is_connected():
+    #     await voice.disconnect()
+    # else:
+    #     await ctx.send("The bot is not connected to a voice channel.")
 
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if client.voice_clients :
+        await voice.disconnect()
+    else :
+        await ctx.send("The bot is not connected to a voice channel.")
 
 @client.command()
 async def pause(ctx):
