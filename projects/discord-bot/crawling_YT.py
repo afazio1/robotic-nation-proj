785394c16494671ca9d15fc6a2d14220e0b9ab64
lhh9799 <77262005+lhh9799@users.noreply.github.com>
2021-01-20 17:27:21 +0900

    중간백업

--- projects/discord-bot/crawling_YT.py
+++ projects/discord-bot/crawling_YT.py
@@ -7,7 +7,7 @@ import time
 #pip install selenium
 #exe파일 경로는 상대경로가 아닌 절대경로로 설정해야함.
 def Crawling_YT_Comment(comm):
-    driver = webdriver.Chrome('C:/Users/BBAEK/Desktop/계절학기/Team Project/Discord_oss_project/projects/discord-bot/chromedriver.exe')
+    driver = webdriver.Chrome('C:/Users/lhh97/Documents/robotic-nation-proj/projects/discord-bot/chromedriver.exe')
     url = comm
     driver.get(url)
 
@@ -61,7 +61,7 @@ def Crawling_YT_Comment(comm):
     return youtube_Ids, youtube_comments, youtube_likes
 
 def Crawling_YT_Title(comm):
-    driver = webdriver.Chrome('C:/Users/BBAEK/Desktop/계절학기/Team Project/Discord_oss_project/projects/discord-bot/chromedriver.exe')
+    driver = webdriver.Chrome('C:/Users/lhh97/Documents/robotic-nation-proj/projects/discord-bot/chromedriver.exe')
     url = "https://www.youtube.com/results?search_query={}".format(comm)
     driver.get(url)
 
@@ -83,3 +83,10 @@ def Crawling_YT_Title(comm):
         return youtube_title[:5], youtube_href[:5]
     else:
         return youtube_title, youtube_href
+
+# a, b = Crawling_YT_Title("아이유")
+# # print(type(a), type(b))
+# # print(a,b)
+# print("a", a)
+# print()
+# print("b", b)
