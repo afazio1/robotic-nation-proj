061542529945fa4032088f6cca11faeaea99a8d9
Alexa Fazio <alexa.fazio04@gmail.com>
2021-04-16 11:29:07 -0400

    added .env setup guide

--- projects/discord-bot/delete-messages.py
+++ projects/discord-bot/delete-messages.py
@@ -11,3 +11,9 @@ async def on_message(message):
 
 
 client.run('YOUR_TOKEN') # copy your token from the developer portal
+
+# If you wish to securely hide your token, you can do so in a .env file.
+# 1. Create a .env in the same directory as your Python scripts
+# 2. In the .env file format your variables like this: VARIABLE_NAME=your_token_here
+# 3. In Python, you can read a .env file using this syntax:
+# token = os.getenv(VARIABLE_NAME)
--- projects/discord-bot/modmail.py
+++ projects/discord-bot/modmail.py
@@ -35,3 +35,9 @@ async def on_message(message):
 
 
 client.run('YOUR_TOKEN')
+
+# If you wish to securely hide your token, you can do so in a .env file.
+# 1. Create a .env in the same directory as your Python scripts
+# 2. In the .env file format your variables like this: VARIABLE_NAME=your_token_here
+# 3. In Python, you can read a .env file using this syntax:
+# token = os.getenv(VARIABLE_NAME)
